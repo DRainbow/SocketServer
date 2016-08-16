2586865b0dee31fb7a75f95e95efbf5e2a079166
Sugar.T <SugarT@SugarTdeMacBook-Pro.local>
2016-08-16 14:46:41 +0800

    添加全局的变量缓存

---
+++ app/src/main/java/com/sugar/android/socketserver/framework/GlobalCache.java
@@ -0,0 +1,12 @@
+package com.sugar.android.socketserver.framework;
+
+/**
+ * @ClassName: GlobalCache
+ * @Description:
+ * @author: SugarT
+ * @date: 16/8/16 上午11:17
+ */
+public class GlobalCache {
+
+    public static double sTargetVersion = 2.0;
+}
